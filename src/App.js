c95ef0534797e63ec83a58019f7f4363fca2dfb3
RajvardhanPalekar <rajvardhan.palekar@gmail.com>
2021-04-17 22:25:10 +0530

    Basic activity tracker frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Activities from "./components/Activities";
+import AddActivity from "./components/AddActivity";
 
 function App() {
+  const [showAdd, setShowAdd] = useState(false);
+
+  const [activities, setActivities] = useState([
+    {
+      id: 1,
+      text: "Buy Groceries",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting with Tony",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Movie night",
+      day: "Feb 5th at 6:30pm",
+      reminder: false,
+    },
+  ]);
+
+  //Add Activity
+  const addActivity = (activity) => {
+    console.log(activity);
+    const id = Math.floor(Math.random() * 10000) + 1;
+
+    const newActivity = { id, ...activity };
+
+    setActivities([...activities, newActivity]);
+  };
+
+  //Delete activity
+  const deleteActivity = (id) => {
+    setActivities(activities.filter((activity) => activity.id !== id));
+  };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setActivities(
+      activities.map((activity) =>
+        activity.id === id
+          ? { ...activity, reminder: !activity.reminder }
+          : activity
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={() => setShowAdd(!showAdd)} showAdd={showAdd} />
+      {showAdd && <AddActivity onAdd={addActivity} />}
+      {activities.length > 0 ? (
+        <Activities
+          activities={activities}
+          onDelete={deleteActivity}
+          onToggle={toggleReminder}
+        />
+      ) : (
+        "No Activities to show"
+      )}
     </div>
   );
 }
---
+++ src/components/Activity.js
@@ -0,0 +1,21 @@
+import { FaTimes } from "react-icons/fa";
+
+const Activity = ({ activity, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`activity ${activity.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(activity.id)}
+    >
+      <h3>
+        {activity.text}{" "}
+        <FaTimes
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(activity.id)}
+        />
+      </h3>
+      <p>{activity.day}</p>
+    </div>
+  );
+};
+
+export default Activity;
